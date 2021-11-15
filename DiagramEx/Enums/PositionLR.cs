72e6e698aed1278fccf2477120650189f958d24e
Platonenkov <platonenkov87@gmail.com>
2021-11-15 08:19:33 +0300

    Position enums

---
+++ DiagramEx/Enums/PositionLR.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagramEx.Enums
+{
+    public enum PositionLR
+    {
+        Left, Right
+    }
+
+    public enum PositionTB
+    {
+        Top,Bottom
+    }
+
+    public enum PositionGeneral
+    {
+        LeftTop,LeftCenter,LeftBottom,
+        RightTop,RightCenter,RightBottom,
+        CenterTop,Center,CenterBottom
+    } 
+}
